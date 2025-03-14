1930fcda7c5dab9e1921916aa8b8aebdf38d4fcc
Nathaniel Butler <nathanielbutler@hotmail.com>
2025-03-14 10:57:13 +0000

    Layout / styling

--- src/pages/Curate.jsx
+++ src/pages/Curate.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deleteExhibition } from '../store/curateSlice';
+import './Curate.css';
 
 function Curate() {
   const dispatch = useDispatch();
@@ -12,21 +13,35 @@ function Curate() {
 
   return (
     <>
-      <Link to="/curate/save-exhibition">Add exhibition</Link>
-      <ul>
+      <nav className="sub-nav">
+        <Link to="/curate/save-exhibition" className="create-exhibition">
+          Create a new exhibition
+        </Link>
+      </nav>
+      <h3>Exhibitions</h3>
+      <ul className="exhibitions-list">
         {exhibitions.map((exhibition) => (
           <li key={exhibition.id}>
             <Link to={`/curate/${exhibition.id}`}>
-              <h4>{exhibition.title}</h4>
+              <h4>
+                {exhibition.title}{' '}
+                <span className="exhibit-count">
+                  ({exhibition.exhibits.length} exhibits)
+                </span>
+              </h4>
             </Link>
             <p>{exhibition.description}</p>
-            <Link to={`/curate/save-exhibition/${exhibition.id}`}>Edit</Link>
-            <button
-              type="button"
-              onClick={() => handleDeleteExhibition(exhibition.id)}
-            >
-              Delete
-            </button>
+            <div className="actions">
+              <Link to={`/curate/save-exhibition/${exhibition.id}`}>
+                Edit details
+              </Link>
+              <button
+                type="button"
+                onClick={() => handleDeleteExhibition(exhibition.id)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
