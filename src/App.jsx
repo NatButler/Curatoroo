d0dfeb0bc7af981168e73f325ab24fcbdf82a888
Nathaniel Butler <nathanielbutler@hotmail.com>
2025-03-19 10:47:19 +0000

    Add layout container

--- src/App.jsx
+++ src/App.jsx
@@ -15,29 +15,31 @@ function App() {
   return (
     <>
       <SkipToContent />
-      <Header />
-      <main id="main" className="main">
-        <Routes>
-          <Route path="*" element={<NotFound />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/explore" element={<Explore />}>
+      <div className="layout-container">
+        <Header />
+        <main id="main" className="main">
+          <Routes>
+            <Route path="*" element={<NotFound />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/explore" element={<Explore />}>
+              <Route
+                path="/explore/collection1"
+                element={<Collection1 />}
+              ></Route>
+              <Route
+                path="/explore/collection2"
+                element={<Collection2 />}
+              ></Route>
+            </Route>
+            <Route path="/curate" element={<Curate />}></Route>
             <Route
-              path="/explore/collection1"
-              element={<Collection1 />}
+              path="/curate/save-exhibition/:id?"
+              element={<SaveExhibition />}
             ></Route>
-            <Route
-              path="/explore/collection2"
-              element={<Collection2 />}
-            ></Route>
-          </Route>
-          <Route path="/curate" element={<Curate />}></Route>
-          <Route
-            path="/curate/save-exhibition/:id?"
-            element={<SaveExhibition />}
-          ></Route>
-          <Route path="/curate/:id" element={<Exhibition />}></Route>
-        </Routes>
-      </main>
+            <Route path="/curate/:id" element={<Exhibition />}></Route>
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
