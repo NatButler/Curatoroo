18e8271e45580ec9e047d65e5181bdbb699c850e
Nathaniel Butler <nathanielbutler@hotmail.com>
2025-03-20 09:54:41 +0000

    Add back link icon

--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import BackArrowIcon from '../assets/arrow_back.svg?react';
 import './BackLink.css';
 
 function BackLink() {
@@ -6,6 +7,7 @@ function BackLink() {
 
   return (
     <button type="button" className="back-link" onClick={() => navigate(-1)}>
+      <BackArrowIcon />
       Back
     </button>
   );
