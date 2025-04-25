ca05a0130bf5b846ceac1e332e4e6468b29cd388
Nathaniel Butler <nathanielbutler@hotmail.com>
2025-04-25 10:28:28 +0100

    Update intro text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
       <p className="instructions">
         This app allows the searching of two museum collections and the curation
         of exhibitions through the selecting of items from each collection.
+        Exhibitions are persisted using your browsers local storage facility.
       </p>
     </>
   );
